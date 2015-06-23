685f63618e0ec0090b186befc7da4e297b667f40
Uzair <uzair_sharif@live.com>
2015-06-23 03:36:23 +0200

    integrated benjamin work

---
+++ work_dir/benjamin/1IP/1IP_GR/SDK/SDK_Workspace/software_prj/Debug/inc/xconvert_to_gray.d
@@ -0,0 +1,43 @@
+inc/xconvert_to_gray.d: ../inc/xconvert_to_gray.c \
+ ../inc/xconvert_to_gray.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../inc/xconvert_to_gray_CONTROL_BUS.h
+
+../inc/xconvert_to_gray.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../inc/xconvert_to_gray_CONTROL_BUS.h:
---
+++ work_dir/benjamin/1IP/ReadMe.txt
@@ -0,0 +1,10 @@
+This folder contains the Xilinx projects containing only one HA:
+
+#1IP_SO
+The Sobel edge-detection function is accelerated on the FPGA.
+
+#1IP_ER
+The image erosion function is accelerated on the FPGA.
+
+#1IP_GR
+The grayscale conversion function is accelerated on the FPGA.
