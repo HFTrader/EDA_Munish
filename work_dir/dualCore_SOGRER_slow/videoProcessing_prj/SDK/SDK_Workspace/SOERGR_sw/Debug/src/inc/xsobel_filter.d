275384ab8fc22f5fd304fcf26d42061bde4f0557
Uzair <uzair_sharif@live.com>
2015-07-01 03:34:11 +0200

    gray-scale HA slowed down to 8MHz -- issue is in detection of gray-scale interrupt after completely processing the frame

---
+++ work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/Debug/src/inc/xsobel_filter.d
@@ -0,0 +1,43 @@
+src/inc/xsobel_filter.d: ../src/inc/xsobel_filter.c \
+ ../src/inc/xsobel_filter.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/inc/xsobel_filter_CONTROL_BUS.h
+
+../src/inc/xsobel_filter.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/inc/xsobel_filter_CONTROL_BUS.h:
---
+++ work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/Debug/src/src/transmitter.d
@@ -0,0 +1,95 @@
+src/src/transmitter.d: ../src/src/transmitter.c \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_types.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_platform.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_types.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_common.h \
+ ../src/src/transmitter.h ../src/src/transmitter_defs.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/tx_lib.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_common.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/edid.h \
+ ../src/src/cf_hdmi.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../src/src/hw_config.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale_CONTROL_BUS.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xsobel_filter.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xsobel_filter_CONTROL_BUS.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/ximage_filter.h \
+ /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/ximage_filter_CONTROL_BUS.h \
+ ../src/src/sw_functions.h ../src/src/global.h
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_types.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_platform.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_types.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_common.h:
+
+../src/src/transmitter.h:
+
+../src/src/transmitter_defs.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/tx_lib.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/atv_common.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/edid.h:
+
+../src/src/cf_hdmi.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../src/src/hw_config.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale_CONTROL_BUS.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xsobel_filter.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xsobel_filter_CONTROL_BUS.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/ximage_filter.h:
+
+/nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/ximage_filter_CONTROL_BUS.h:
+
+../src/src/sw_functions.h:
+
+../src/src/global.h:
