ec16750824eebee40b17863f38b3356bd35c4422
Uzair <uzair_sharif@live.com>
2015-07-15 03:32:51 +0200

    code refactoring in slow HA project

--- work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/Debug/src/src/hwsw_functions.d
+++ work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/Debug/src/src/hwsw_functions.d
@@ -11,11 +11,15 @@ src/src/hwsw_functions.d: ../src/src/hwsw_functions.c \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_printf.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic_hw.h \
+ ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_exception.h \
  /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h \
- ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h \
  ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h \
  /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale_CONTROL_BUS.h \
  /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xsobel_filter.h \
@@ -47,6 +51,16 @@ src/src/hwsw_functions.d: ../src/src/hwsw_functions.c \
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xparameters_ps.h:
 
+../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xscugic_hw.h:
+
+../../standalone_bsp_0/ps7_cortexa9_0/include/xil_exception.h:
+
 /nfs/TUEIEDAprojects/SystemDesign/work/zynq/mamatha/uzair_sharif/EDA_Munish/work_dir/dualCore_SOGRER_slow/videoProcessing_prj/SDK/SDK_Workspace/SOERGR_sw/src/inc/xgray_scale.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_types.h:
@@ -54,8 +68,6 @@ src/src/hwsw_functions.d: ../src/src/hwsw_functions.c \
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xstatus.h:
-
-../../standalone_bsp_0/ps7_cortexa9_0/include/xil_assert.h:
 
 ../../standalone_bsp_0/ps7_cortexa9_0/include/xil_io.h:
 
